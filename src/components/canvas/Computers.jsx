5d4ed73c0bfd96cf086c3bc4399bb179544a8def
Vincent Varghese <vinxent890@gmail.com>
2023-07-25 00:44:01 +0530

    3d

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -21,7 +21,7 @@ const Computers = ({ isMobile }) => {
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 1.8 : 2.5}
+        scale={isMobile ? 0.2 : 2.5}
         position={isMobile ? [0, -3, -0.5] : [5, -3.25, -2.3]}
         rotation={[-0.01, 0.8, -0.1]}
       />
