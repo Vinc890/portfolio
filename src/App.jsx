0cc3974d8300bca3988dfc99a2993c385dba9107
Vincent Varghese <vinxent890@gmail.com>
2026-05-28 19:47:05 +0530

    Size of hero 3d element

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,52 @@
 import { BrowserRouter } from "react-router-dom";
 
-import { About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas, CinematicCanvas } from "./components";
+import {
+  About,
+  Contact,
+  Experience,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+  CinematicCanvas,
+} from "./components";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className='relative z-0 bg-primary min-h-screen text-white overflow-hidden'>
-        {/* Full-screen fixed background cinematic 3D canvas */}
+      <div className="relative z-0 bg-primary min-h-screen text-white overflow-hidden">
         <CinematicCanvas />
-        
+
         <Navbar />
-        
-        {/* Overlay structure */}
-        <div className='relative z-10 bg-transparent'>
+
+        <div className="relative z-10 bg-transparent">
           <Hero />
         </div>
-        
-        <div className='relative z-10 bg-transparent'>
+
+        <div className="relative z-10 bg-transparent">
           <About />
         </div>
-        
-        <div className='relative z-10 bg-transparent'>
+
+        <div className="relative z-10 bg-transparent">
           <Experience />
         </div>
-        
-        <div className='relative z-10 bg-transparent'>
+
+        <div className="relative z-10 bg-transparent">
           <Tech />
         </div>
-        
-        <div className='relative z-10 bg-transparent'>
+
+        <div className="relative z-10 bg-transparent">
           <Works />
         </div>
-        
-        <div className='relative z-10 bg-transparent'>
+
+        <div className="relative z-10 bg-transparent">
           <Contact />
           <StarsCanvas />
         </div>
       </div>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -8,10 +8,8 @@ const Computers = ({ isMobile }) => {
 
   return (
     <mesh>
-      {/* Cool, professional titanium ambient light base */}
       <hemisphereLight intensity={0.2} groundColor="black" color="#E2E8F0" />
 
-      {/* High-intensity crisp white keylight */}
       <spotLight
         position={[-15, 35, 15]}
         angle={0.2}
@@ -22,7 +20,6 @@ const Computers = ({ isMobile }) => {
         shadow-mapSize={1024}
       />
 
-      {/* Soft brushed-titanium silver secondary light for metal sheen */}
       <spotLight
         position={[25, 20, -10]}
         angle={0.35}
@@ -33,13 +30,12 @@ const Computers = ({ isMobile }) => {
         shadow-mapSize={1024}
       />
 
-      {/* Subtle cooling point light */}
       <pointLight intensity={1.2} color="#F8FAFC" position={[0, -1, 3]} />
 
       <primitive
         object={computer.scene}
-        scale={isMobile ? 1.9 : 1.6}
-        position={isMobile ? [0, -1.6, 0] : [1.2, -1.4, 0.5]}
+        scale={isMobile ? 1.4 : 1.6}
+        position={isMobile ? [1, -1, 0.2] : [1.2, -1.4, 0.5]}
         rotation={[-0.01, 0.8, -0.1]}
       />
     </mesh>
